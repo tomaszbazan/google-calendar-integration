839b75d88d9823c8bdf51c0cc3d73b117bab5ff0
Marcin Bazan <you@example.com>
2021-09-03 13:23:13 +0200

    project init

---
+++ src/main/java/pl/tmbazan/googlecalendarintegration/GoogleCalendarIntegrationApplication.java
@@ -0,0 +1,13 @@
+package pl.tmbazan.googlecalendarintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCalendarIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tmbazan/googlecalendarintegration/GoogleCalendarIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tmbazan.googlecalendarintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCalendarIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
